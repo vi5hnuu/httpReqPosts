658b4831e0911595cf4dbed80b832b97a1659468
vi5hnuu <vishnu.12017539@lpu.in>
2022-10-03 21:50:45 +0530

    div->form

--- asset/js/script.js
+++ asset/js/script.js
@@ -10,7 +10,8 @@ descEl.addEventListener('keypress', (evnt) => {
     }
 })
 
-btnAdd.addEventListener('click', () => {
+btnAdd.addEventListener('click', (evnt) => {
+    evnt.preventDefault()
     const titleEl = document.getElementById('title')
     const descEl = document.getElementById('description')
     const postEl = document.importNode(postTemp.content, true).firstElementChild
@@ -34,26 +35,35 @@ posts.addEventListener('click', (evnt) => {
     }
 })
 
-btnFetch.addEventListener('click', () => {
-    posts.innerHTML = ''
-    document.querySelector('.spinner').classList.remove('hidden')
-    const xhr = new XMLHttpRequest()
-    xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts', async = true)
+
+function getResponseData() {
+    const promise = new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest()
+        xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts', async = true)
 
 
-    xhr.addEventListener('load', (evnt) => {
-        document.querySelector('.spinner').classList.add('hidden')
-        const postsData = JSON.parse(xhr.response);
-        // console.log(postsData);
-        postsData.forEach(postDt => {
-            const postEl = document.importNode(postTemp.content, true).firstElementChild
-            // console.dir(postEl.querySelector('.post-title'));
-
-            postEl.querySelector('.post-title').innerText = postDt['title']
-            postEl.querySelector('.post-description').innerText = postDt['body']
-            posts.insertAdjacentElement('beforeEnd', postEl)
-        });
+        xhr.addEventListener('load', (evnt) => {
+            document.querySelector('.spinner').classList.add('hidden')
+            resolve(xhr.response);
+        })
+        xhr.send()
     })
-    xhr.send()
+    return promise
+}
+btnFetch.addEventListener('click', async (evnt) => {
+    posts.innerHTML = ''
+    evnt.target.disabled = true
+    document.querySelector('.spinner').classList.remove('hidden')
+    const data = await getResponseData()
+    const postsData = JSON.parse(data)
+    postsData.forEach(postDt => {
+        const postEl = document.importNode(postTemp.content, true).firstElementChild
+        // console.dir(postEl.querySelector('.post-title'));
+
+        postEl.querySelector('.post-title').innerText = postDt['title']
+        postEl.querySelector('.post-description').innerText = postDt['body']
+        posts.insertAdjacentElement('beforeEnd', postEl)
+    });
+    evnt.target.disabled = false
 })
 
